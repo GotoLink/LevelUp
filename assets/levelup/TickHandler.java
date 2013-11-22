6b5bdca76b631a1b8a53ed8828fdf9ab87353e80
GotoLink <gotolinkminecraft@gmail.com>
2013-11-22 18:42:30 +0100

    Added support for custom planks for wood logs
    and support for custom soils

--- assets/levelup/TickHandler.java
+++ assets/levelup/TickHandler.java
@@ -10,17 +10,20 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
-import net.minecraft.block.BlockDirt;
 import net.minecraft.block.BlockGravel;
 import net.minecraft.block.BlockLog;
 import net.minecraft.block.BlockOre;
 import net.minecraft.block.BlockRedstoneOre;
 import net.minecraft.block.BlockStem;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.inventory.ContainerPlayer;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
 import cpw.mods.fml.common.ITickHandler;
@@ -28,6 +31,36 @@ import cpw.mods.fml.common.TickType;
 
 public class TickHandler implements ITickHandler {
 	public static Collection<BlockPosition> blockClicked = new HashSet();
+	public static Map<Integer, Integer> blockToCounter = new HashMap();
+	static {
+		blockToCounter.put(Block.oreCoal.blockID, 0);
+		blockToCounter.put(Block.oreLapis.blockID, 1);
+		blockToCounter.put(Block.oreRedstone.blockID, 2);
+		blockToCounter.put(Block.oreIron.blockID, 3);
+		blockToCounter.put(Block.oreGold.blockID, 4);
+		blockToCounter.put(Block.oreEmerald.blockID, 5);
+		blockToCounter.put(Block.oreDiamond.blockID, 6);
+		blockToCounter.put(Block.oreNetherQuartz.blockID, 7);
+	}
+	private static ItemStack digLoot[] = { new ItemStack(Item.clay, 8), new ItemStack(Item.bowlEmpty, 2), new ItemStack(Item.coal, 4), new ItemStack(Item.painting), new ItemStack(Item.stick, 4),
+			new ItemStack(Item.silk, 2) };
+	private static ItemStack digLoot1[] = { new ItemStack(Item.swordStone), new ItemStack(Item.shovelStone), new ItemStack(Item.pickaxeStone), new ItemStack(Item.axeStone) };
+	private static ItemStack digLoot2[] = { new ItemStack(Item.slimeBall, 2), new ItemStack(Item.redstone, 8), new ItemStack(Item.ingotIron), new ItemStack(Item.ingotGold) };
+	private static ItemStack digLoot3[] = { new ItemStack(Item.diamond) };
+
+	@Override
+	public String getLabel() {
+		return "LevelUpTick";
+	}
+
+	@Override
+	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+	}
+
+	@Override
+	public EnumSet<TickType> ticks() {
+		return EnumSet.of(TickType.SERVER);
+	}
 
 	@Override
 	public void tickStart(EnumSet<TickType> type, Object... tickData) {
@@ -57,13 +90,30 @@ public class TickHandler implements ITickHandler {
 		}
 	}
 
+	public static int getSkill(EntityPlayer player, int id) {
+		return PlayerExtendedProperties.getSkillFromIndex(player, id);
+	}
+
+	private static ItemStack getPlanks(EntityPlayerMP player, Block block, int meta) {
+		if (block.blockID != Block.wood.blockID) {
+			InventoryCrafting craft = new ContainerPlayer(player.inventory, player.worldObj.isRemote, player).craftMatrix;
+			craft.setInventorySlotContents(0, new ItemStack(block, 1, meta));
+			ItemStack planks = CraftingManager.getInstance().findMatchingRecipe(craft, player.worldObj);
+			if (planks != null) {
+				planks.stackSize = 2;
+				return planks;
+			}
+		}
+		return new ItemStack(Block.planks, 2, meta & 3);
+	}
+
 	private static void onBlockBreak(World world, EntityPlayerMP player, BlockPosition info) {
 		Block block = Block.blocksList[info.getData()[5]];
 		int meta = info.getData()[6];
 		//System.out.println("Broken "+block.getUnlocalizedName());
 		int skill;
 		Random random = new Random();
-		if (block instanceof BlockDirt) {
+		if (block.blockMaterial == Material.ground) {
 			skill = getSkill(player, 11);
 			if (random.nextFloat() <= skill / 200F) {
 				ItemStack[] aitemstack4 = digLoot;
@@ -101,7 +151,7 @@ public class TickHandler implements ITickHandler {
 				world.spawnEntityInWorld(new EntityItem(world, info.getData()[2], info.getData()[3], info.getData()[4], new ItemStack(Item.stick, 2)));
 			}
 			if (random.nextDouble() <= skill / 150D) {
-				world.spawnEntityInWorld(new EntityItem(world, info.getData()[2], info.getData()[3], info.getData()[4], new ItemStack(Block.planks, 2, meta & 3)));
+				world.spawnEntityInWorld(new EntityItem(world, info.getData()[2], info.getData()[3], info.getData()[4], getPlanks(player, block, meta)));
 			}
 		} else if (block instanceof BlockOre || block instanceof BlockRedstoneOre) {
 			skill = getSkill(player, 0);
@@ -120,41 +170,5 @@ public class TickHandler implements ITickHandler {
 				world.spawnEntityInWorld(new EntityItem(world, info.getData()[2], info.getData()[3], info.getData()[4], new ItemStack(ID, 1, 0)));
 			}
 		}
-	}
-
-	public static Map<Integer, Integer> blockToCounter = new HashMap();
-	static {
-		blockToCounter.put(Block.oreCoal.blockID, 0);
-		blockToCounter.put(Block.oreLapis.blockID, 1);
-		blockToCounter.put(Block.oreRedstone.blockID, 2);
-		blockToCounter.put(Block.oreIron.blockID, 3);
-		blockToCounter.put(Block.oreGold.blockID, 4);
-		blockToCounter.put(Block.oreEmerald.blockID, 5);
-		blockToCounter.put(Block.oreDiamond.blockID, 6);
-		blockToCounter.put(Block.oreNetherQuartz.blockID, 7);
-	}
-
-	public static int getSkill(EntityPlayer player, int id) {
-		return PlayerExtendedProperties.getSkillFromIndex(player, id);
-	}
-
-	private static ItemStack digLoot[] = { new ItemStack(Item.clay, 8), new ItemStack(Item.bowlEmpty, 2), new ItemStack(Item.coal, 4), new ItemStack(Item.painting), new ItemStack(Item.stick, 4),
-		new ItemStack(Item.silk, 2) };
-	private static ItemStack digLoot1[] = { new ItemStack(Item.swordStone), new ItemStack(Item.shovelStone), new ItemStack(Item.pickaxeStone), new ItemStack(Item.axeStone) };
-	private static ItemStack digLoot2[] = { new ItemStack(Item.slimeBall, 2), new ItemStack(Item.redstone, 8), new ItemStack(Item.ingotIron), new ItemStack(Item.ingotGold) };
-	private static ItemStack digLoot3[] = { new ItemStack(Item.diamond) };
-
-	@Override
-	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
-	}
-
-	@Override
-	public EnumSet<TickType> ticks() {
-		return EnumSet.of(TickType.SERVER);
-	}
-
-	@Override
-	public String getLabel() {
-		return "LevelUpTick";
 	}
 }
