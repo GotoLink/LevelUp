ee25ecf45db0702cf14b513cd150b4fd00a27f5d
GotoLink <gotolinkminecraft@gmail.com>
2014-12-27 01:58:11 +0100

    Improved bow support for archer skill

--- assets/levelup/BowEventHandler.java
+++ assets/levelup/BowEventHandler.java
@@ -1,43 +1,44 @@
 package assets.levelup;
 
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
-import net.minecraft.init.Items;
+import net.minecraft.item.EnumAction;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.event.entity.player.ArrowNockEvent;
+import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
 
 public final class BowEventHandler {
     public static final BowEventHandler INSTANCE = new BowEventHandler();
-    private BowEventHandler(){}
 
-	@SubscribeEvent
-	public void onSpawn(EntityJoinWorldEvent event) {
-		if (event.entity instanceof EntityArrow) {
-			EntityArrow arrow = (EntityArrow) event.entity;
-			if (arrow.shootingEntity instanceof EntityPlayer) {
-				int archer = getArcherSkill((EntityPlayer) arrow.shootingEntity);
-				if (archer != 0) {
-					arrow.motionX *= 1.0F + archer / 100F;
-					arrow.motionY *= 1.0F + archer / 100F;
-					arrow.motionZ *= 1.0F + archer / 100F;
-				}
-			}
-		}
-	}
+    private BowEventHandler() {
+    }
 
-	@SubscribeEvent
-	public void onBowUse(ArrowNockEvent event) {
-		int archer = getArcherSkill(event.entityPlayer);
-		if (archer != 0) {
-			if (event.entityPlayer.capabilities.isCreativeMode || event.entityPlayer.inventory.hasItem(Items.arrow)) {
-				event.entityPlayer.setItemInUse(event.result, event.result.getMaxItemUseDuration() - archer / 5);
-			}
-			event.setCanceled(true);
-		}
-	}
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void onSpawn(EntityJoinWorldEvent event) {
+        if (event.entity instanceof EntityArrow) {
+            EntityArrow arrow = (EntityArrow) event.entity;
+            if (arrow.shootingEntity instanceof EntityPlayer) {
+                int archer = getArcherSkill((EntityPlayer) arrow.shootingEntity);
+                if (archer != 0) {
+                    arrow.motionX *= 1.0F + archer / 100F;
+                    arrow.motionY *= 1.0F + archer / 100F;
+                    arrow.motionZ *= 1.0F + archer / 100F;
+                }
+            }
+        }
+    }
 
-	public static int getArcherSkill(EntityPlayer player) {
-		return PlayerExtendedProperties.getSkillFromIndex(player, 5);
-	}
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void onBowUse(PlayerUseItemEvent.Start event) {
+        if (event.item != null && event.item.getMaxStackSize() == 1 && event.item.getItemUseAction() == EnumAction.bow) {
+            int archer = getArcherSkill(event.entityPlayer);
+            if (archer != 0 && event.duration > archer / 5)
+                event.duration -= (archer / 5);
+        }
+    }
+
+    public static int getArcherSkill(EntityPlayer player) {
+        return PlayerExtendedProperties.getSkillFromIndex(player, 5);
+    }
 }
