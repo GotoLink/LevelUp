5575645db7a2dd4a775db7c37153134edecead90
GotoLink <gotolinkminecraft@gmail.com>
2014-12-27 02:01:44 +0100

    Made subtypes for unlearning book

--- assets/levelup/ItemRespecBook.java
+++ assets/levelup/ItemRespecBook.java
@@ -1,28 +1,45 @@
 package assets.levelup;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public final class ItemRespecBook extends Item {
-	public static boolean resClass = false;
+    public ItemRespecBook() {
+        super();
+        setHasSubtypes(true);
+    }
 
-	public ItemRespecBook() {
-		super();
-	}
-
-	@Override
-	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer) {
-        if(!world.isRemote){
-            PlayerExtendedProperties.getSkillMap(entityplayer).put("XP", PlayerExtendedProperties.getSkillPoints(entityplayer));
-            if (resClass)
-                PlayerExtendedProperties.setPlayerClass(entityplayer, (byte) 0);
-            PlayerExtendedProperties.resetSkills(entityplayer, false);
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer) {
+        if (!world.isRemote) {
+            PlayerExtendedProperties.from(entityplayer).convertPointsToXp(itemstack.getItemDamage() > 0);
             FMLEventHandler.INSTANCE.loadPlayer(entityplayer);
         }
-		if (!entityplayer.capabilities.isCreativeMode)
-			itemstack.stackSize--;
-		return itemstack;
-	}
+        if (!entityplayer.capabilities.isCreativeMode)
+            itemstack.stackSize--;
+        return itemstack;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(Item item, CreativeTabs tab, List list) {
+        super.getSubItems(item, tab, list);
+        list.add(new ItemStack(item, 1, 1));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean isAdvanced) {
+        if (itemStack.getItemDamage() > 0) {
+            list.add(StatCollector.translateToLocal("respecbook.canresetclass"));
+        }
+    }
 }
